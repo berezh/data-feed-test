02fa53501e3bc8fa8eb20af51bb830a7eb99285c
andriirak <andrii.rack@gmail.com>
2021-09-30 10:23:33 +0300

    remove redux saga #13

--- src/data-feed/index.d.ts
+++ src/data-feed/index.d.ts
@@ -1,9 +1,6 @@
-import { feedReducer } from './redux/reducer';
-import { feedSaga } from './redux/saga';
 export * from './interfaces';
 export * from './feed';
 export * from './light-feed';
 export * from './rows';
 export * from './utils';
 export * from './texts';
-export { feedReducer, feedSaga };
--- src/redux/saga.ts
+++ src/redux/saga.ts
@@ -2,9 +2,7 @@ import { fork, all } from 'redux-saga/effects';
 import { SagaIterator } from 'redux-saga';
 
 import { generalSaga } from '../units/general/redux';
-import { feedSaga } from 'src/data-feed';
 
 export function* RootSaga(): SagaIterator {
-    yield all([fork(feedSaga)]);
     yield all([fork(generalSaga)]);
 }
